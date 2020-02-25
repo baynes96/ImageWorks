25f9af04a7d1e0bd49fc792935f2ffe570c210e2
bayneslc <bayneslc@LCBLAB2110-20.uccob.uc.edu>
2020-02-25 09:06:54 -0500

    ImagWork

---
+++ InClass_2_25_2020/Src/image_functions.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 25, 2020
+
+@author: nicomp
+'''
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+#import requests
+from io import BytesIO
+
+def load_image( filename ) :
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+        return myimage
+    except:
+        print:("Load _Image(): Unable to open" + filename)
+        return None
+    
